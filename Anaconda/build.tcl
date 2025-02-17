d7620287a786b3d8f25ac68f144fd3ab603fb512
Robert Soliday <soliday@Macmini.attlocal.net>
2025-02-17 11:03:17 -0600

    Updated for MacOS

--- Anaconda/build.tcl
+++ Anaconda/build.tcl
@@ -12,6 +12,7 @@ if {$tcl_platform(os) == "Linux"} {
     file copy -force ../lib/Darwin-x86_64/libsddsdata.so src/sdds/sddsdata.so
     set files {"sddsdata.so"}
 } elseif {($tcl_platform(os) == "Darwin") && ($tcl_platform(machine) == "arm64")} {
+    set pyreq ">=3.8"
     set binDir /Users/soliday/miniconda3/bin
     file copy -force ../lib/Darwin-arm64/libsddsdata.so src/sdds/sddsdata.so
     set files {"sddsdata.so"}
@@ -30,23 +31,42 @@ catch {exec ${binDir}/conda build purge}
 catch {exec ${binDir}/conda build purge-all}
 
 
-    puts "Building SDDS Conda Package for Python"
-    
-    file delete -force src/setup.py src/meta.yaml
-    if {($tcl_platform(os) == "Linux")} {
-        exec ../../SDDS/bin/Linux-x86_64/replaceText src/setup.py.template src/setup.py \
-          -orig=<VERSION>,<PYFILES> -repl=${version},${files}
-        exec ../../SDDS/bin/Linux-x86_64/replaceText src/meta.yaml.template src/meta.yaml \
-          -orig=<VERSION>,<LIBGCC> "-repl=${version},- libgcc-ng"
-    
-    }
-puts "\nManually run:"
-puts "${binDir}/conda-build . --package-format=.conda ; rm -f src/sdds/meta.yaml src/sdds/setup.py src/sdds/sdds.py src/sdds/*.dll src/sdds/*.pyd src/sdds/*.so"
+puts "Building SDDS Conda Package for Python"
 
-#    if {[catch {exec ${binDir}/conda-build . --python=$version --package-format=.conda} results]} {
-#        puts "ERROR: $results"
-#    }
-#    file delete sdds/conda_build_config.yaml
-#eval file delete src/meta.yaml src/setup.py src/sdds/sdds.py [glob -nocomplain src/sdds/*.dll src/sdds/*.pyd src/sdds/*.so]
-#}
+file delete -force src/setup.py src/meta.yaml
+if {($tcl_platform(os) == "Linux")} {
+    exec ../../SDDS/bin/Linux-x86_64/replaceText src/setup.py.template src/setup.py \
+	-orig=<VERSION>,<PYFILES> -repl=${version},${files}
+    exec ../../SDDS/bin/Linux-x86_64/replaceText src/meta.yaml.template src/meta.yaml \
+	-orig=<VERSION>,<LIBGCC> "-repl=${version},- libgcc-ng"
+    exec ../../SDDS/bin/Linux-x86_64/replaceText src/conda_build_config.yaml.template \
+	src/conda_build_config.yaml \
+	-orig=<VER37> "-repl=  - 3.7"
+} elseif {($tcl_platform(os) == "Darwin") && ($tcl_platform(machine) == "x86_64")} {
+    exec ../../SDDS/bin/Darwin-x86_64/replaceText src/setup.py.template src/setup.py \
+	-orig=<VERSION>,<PYFILES> -repl=${version},${files}
+    exec ../../SDDS/bin/Darwin-x86_64/replaceText src/meta.yaml.template src/meta.yaml \
+	-orig=<VERSION>,<LIBGCC> "-repl=${version},"
+    exec ../../SDDS/bin/Darwin-x86_64/replaceText src/conda_build_config.yaml.template \
+	src/conda_build_config.yaml \
+	-orig=<VER37> "-repl=  - 3.7"
+} elseif {($tcl_platform(os) == "Darwin") && ($tcl_platform(machine) == "arm64")} {
+    exec ../../SDDS/bin/Darwin-arm64/replaceText src/setup.py.template src/setup.py \
+	-orig=<VERSION>,<PYFILES> -repl=${version},${files}
+    exec ../../SDDS/bin/Darwin-arm64/replaceText src/meta.yaml.template src/meta.yaml \
+	-orig=<VERSION>,<LIBGCC> "-repl=${version},"
+    exec ../../SDDS/bin/Darwin-arm64/replaceText src/conda_build_config.yaml.template \
+	src/conda_build_config.yaml \
+	-orig=<VER37> "-repl="
+} elseif {($tcl_platform(os) == "Windows NT")} {
+    exec ../../SDDS/bin/Windows-x64/replaceText src/setup.py.template src/setup.py \
+	-orig=<VERSION>,<PYFILES> -repl=${version},${files}
+    exec ../../SDDS/bin/Windows-x64/replaceText src/meta.yaml.template src/meta.yaml \
+	-orig=<VERSION>,<LIBGCC> "-repl=${version},"
+    exec ../../SDDS/bin/Windows-x64/replaceText src/conda_build_config.yaml.template \
+	src/conda_build_config.yaml \
+	-orig=<VER37> "-repl=  - 3.7"
+}
+puts "\nManually run:"
+puts "${binDir}/conda-build . --package-format=.conda ; rm -f src/conda_build_config.yaml  src/meta.yaml src/setup.py src/sdds/sdds.py [glob -nocomplain src/sdds/*.dll src/sdds/*.pyd src/sdds/*.so]"
 
